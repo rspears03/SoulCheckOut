026745c65fd4a375d3e008264a26c3e86ee781bb
Rodney Spears <rodneyspears03@gmail.com>
2020-06-12 12:49:07 -0500

    Pushing dataup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3010;
+const port = process.env.port || 8080
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const connection = require('./config');
